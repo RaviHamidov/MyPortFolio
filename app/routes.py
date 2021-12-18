ba785a4ddfea849636d8eb7ea77732986ca2014a
Ravi Hamidov <ravihamidov42@gmail.com>
2021-12-18 19:05:57 +0400

    Progress . . .

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask.templating import render_template
 from run import app
 
 @app.route('/')
-def index():
+def app_index():
     from models import About
     about = About.query.all()
     return render_template('app/index.html',about=about)
