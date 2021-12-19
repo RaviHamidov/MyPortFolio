d1ef68c551c3a33c9a730c4c4998ce2020e73016
Ravi Hamidov <ravihamidov42@gmail.com>
2021-12-19 05:09:24 +0400

    Progress . . .

--- run.py
+++ run.py
@@ -6,15 +6,15 @@ from flask_login import LoginManager, UserMixin, login_manager, login_user, logi
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-UPLOAD_FOLDER = 'static/assets/uploads'
+UPLOAD_FOLDER = "static/assets/uploads"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587   
-app.config['MAIL_USERNAME'] = "sabuhiq0@gmail.com"
-app.config['MAIL_PASSWORD'] = "Sabuhi07123"
+app.config['MAIL_USERNAME'] = "ravihamidov42@gmail.com"
+app.config['MAIL_PASSWORD'] = "neyse6734"
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
